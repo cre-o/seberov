bf38b96964bcc70f5d435e9ba733d1b0cfbc63d7
laminelli <extralinedesign@gmail.com>
2015-06-08 13:53:41 +0200

    Fix values.

--- vendor/extensions/apartments/app/models/refinery/apartments/apartment.rb
+++ vendor/extensions/apartments/app/models/refinery/apartments/apartment.rb
@@ -13,15 +13,15 @@ module Refinery
       scope :by_expensiveness, -> { order(price: :desc) }
       scope :by_position, -> { order(position: :asc) }
 
-      scope :block_a, -> { where(block: 'a') }
-      scope :block_b, -> { where(block: 'b') }
-      scope :block_c, -> { where(block: 'c') }
-      scope :block_d, -> { where(block: 'd') }
+      scope :block_a, -> { where('LOWER(block) = :letter', letter: 'a') }
+      scope :block_b, -> { where('LOWER(block) = :letter', letter: 'b') }
+      scope :block_c, -> { where('LOWER(block) = :letter', letter: 'c') }
+      scope :block_d, -> { where('LOWER(block) = :letter', letter: 'd') }
 
       accepts_nested_attributes_for :apartment_floors, :reject_if => :all_blank, :allow_destroy => true
 
       validates :unit_id, presence: true, uniqueness: true
-      validates :block, presence: true
+      validates :block, presence: true, inclusion: { in: BLOCKS, case_insensitive: true, message: "Not in #{BLOCKS} list" }
       validates :floor, presence: true, numericality: true
       validates :total_floors, presence: true, numericality: true
       validates :state, inclusion: { in: Refinery::Apartments::Apartment::STATES }
@@ -29,6 +29,12 @@ module Refinery
       # To enable admin searching, add acts_as_indexed on searchable fields, for example:
       #
       acts_as_indexed :fields => [:unit_id]
+
+      before_validation :downcase
+
+      def downcase
+        self.block = block.downcase
+      end
     end
   end
 end

7b864114a33264ea152d5d6a3f6328430e0e9e30
laminelli <extralinedesign@gmail.com>
2015-06-08 13:27:20 +0200

    Serializer.

---
+++ vendor/extensions/apartments/app/serializers/refinery/apartments/apartment_serializer.rb
@@ -0,0 +1,14 @@
+module Refinery
+  module Apartments
+    class ApartmentSerializer < ActiveModel::Serializer
+      include ActionView::Helpers::NumberHelper
+      include ApartmentDecorations
+
+      attributes :id, :unit_id, :block, :photo, :floor, :floors, :total_floors,
+        :layout, :apartment_area, :balcony_terace_area, :front_yard_area,
+        :price, :price_formated, :position, :state
+
+      has_many :apartment_floors
+    end
+  end
+end

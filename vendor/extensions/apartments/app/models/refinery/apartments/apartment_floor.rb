f76de97bc0a7d53cbf51b3715e747f4236314dae
laminelli <extralinedesign@gmail.com>
2015-06-15 17:44:03 +0200

    Fixes.

--- vendor/extensions/apartments/app/models/refinery/apartments/apartment_floor.rb
+++ vendor/extensions/apartments/app/models/refinery/apartments/apartment_floor.rb
@@ -4,7 +4,7 @@ module Refinery
       self.table_name = 'refinery_apartment_floors'
       has_one :apartment
 
-      validates :floor_number, presence: true
+      validates :floor_number, presence: true, uniqueness: { scope: :apartment_id }
 
       # To enable admin searching, add acts_as_indexed on searchable fields, for example:
       #

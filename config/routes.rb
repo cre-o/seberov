7fb3d50b99e471b1a246cb6ff249def94280d848
laminelli <extralinedesign@gmail.com>
2015-07-09 08:27:16 +0200

    Up!

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   # configuration option `mounted_path` to something different in config/initializers/refinery/core.rb
   #
   # We ask that you don't use the :as option here, as Refinery relies on it being the default of "refinery"
-  resources :translations, at: '/', only: :show
   post '/callback', to: 'application#callback'
   root to: 'refinery/pages#home'
 
